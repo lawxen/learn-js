9d02e119d1c7bc2c4dc21030daca98e5bcb3c64b
Lawxen Liu <lawxen@outlook.com>
2022-07-06 00:20:09 +0800

    Update

--- javascript_the_definitive_guide/1.Introduction to JavaScript/main.js
+++ javascript_the_definitive_guide/1.Introduction to JavaScript/main.js
@@ -30,16 +30,16 @@ function plus1(x) { // Define a function named "plus1" with parameter "x"
     return x + 1; // Return a value one larger than the value passed in 
 } // Functions are enclosed in curly braces 
 plus1(y) // => 4: y is 3, so this invocation returns 3+1 
-let square = function (x) { // Functions are values and can be assigned to vars
+let square1 = function (x) { // Functions are values and can be assigned to vars
     return x * x; // Compute the function's value 
 }; // Semicolon marks the end of the assignment. 
-square(plus1(y)) // => 16: invoke two functions in one expression
+square1(plus1(y)) // => 16: invoke two functions in one expression
 
 // Arrow functions
 const plus1 = x => x + 1; // The input x maps to the output x + 1 
-const square = x => x * x; // The input x maps to the output x * x 
+const square2 = x => x * x; // The input x maps to the output x * x 
 plus1(y) // => 4: function invocation is the same 
-square(plus1(y)) // => 16
+square2(plus1(y)) // => 16
 
 
 console.log("*********** Class **************")
